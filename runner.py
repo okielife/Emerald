3c7beeeeb6fcbdc41e6f93ae632f59be4ae9eff0
Edwin Lee <leeed2001@gmail.com>
2023-06-01 13:36:05 -0500

    Update to E+ 23.1 and use EnergyPlusAPIHelper

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import distutils.cmd
+from energyplus_api_helpers.import_helper import EPlusAPIHelper
 from json import loads
 import os
 from pathlib import Path
@@ -27,21 +28,22 @@ class Runner(distutils.cmd.Command):
         pass
 
     def run(self):
-        if os.path.exists("/eplus/installs/EnergyPlus-22-1-0/"):
+        version_dotted = '23.1.0'  # the E+ release number to use as X.Y.Z
+        version_sha = '87ed9199d4'  # the release SHA as a 10 digit hex
+        ubuntu_ver = '20.04'  # the version of Ubuntu built on GitHub Actions, used in the built file name
+        version_dashed = version_dotted.replace('.', '-')
+        if os.path.exists(f"/eplus/installs/EnergyPlus-{version_dashed}/"):
             # if I am running locally, just use my local install
-            ep_install_path = "/eplus/installs/EnergyPlus-22-1-0/"
+            ep_install_path = f"/eplus/installs/EnergyPlus-{version_dashed}/"
         else:
-            file_name = 'EnergyPlus-22.1.0-ed759b17ee-Linux-Ubuntu20.04-x86_64.tar.gz'
-            url = 'https://github.com/NREL/EnergyPlus/releases/download/v22.1.0/%s' % file_name
+            file_name = f'EnergyPlus-{version_dotted}-{version_sha}-Linux-Ubuntu{ubuntu_ver}-x86_64.tar.gz'
+            url = f"https://github.com/NREL/EnergyPlus/releases/download/v{version_dotted}/{file_name}"
             extract_dir = Path(tempfile.mkdtemp())
             ep_tar_path = extract_dir / file_name
             _, headers = urllib.request.urlretrieve(url, ep_tar_path)
             extract_command = ['tar', '-xzf', file_name, '-C', extract_dir]
             check_call(extract_command, cwd=extract_dir)
-            ep_install_path = extract_dir / 'EnergyPlus-22.1.0-ed759b17ee-Linux-Ubuntu20.04-x86_64'
-
-        sys.path.insert(0, str(ep_install_path))
-        from pyenergyplus.api import EnergyPlusAPI
+            ep_install_path = extract_dir / f"EnergyPlus-{version_dotted}-{version_sha}-Linux-Ubuntu{ubuntu_ver}-x86_64"
 
         # set up a run dir
         this_file_path = Path(os.path.realpath(__file__))
@@ -57,7 +59,8 @@ class Runner(distutils.cmd.Command):
             f.write(full_idf_string)
 
         # initialize the API and generate a state object
-        api = EnergyPlusAPI()
+        e = EPlusAPIHelper(ep_install_path)
+        api = e.get_api_instance()
         state = api.state_manager.new_state()
 
         # register callbacks to allow weather data actuation
